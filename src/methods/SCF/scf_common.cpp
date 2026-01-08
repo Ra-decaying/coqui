8488c76efe20909c97c8b9ef3e66c6e24516f47b
Jianhao Li <jianhaol@umich.edu>
2026-01-08 18:02:56 +0800

    solve conflict

--- src/methods/SCF/scf_common.cpp
+++ src/methods/SCF/scf_common.cpp
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ double compute_Nelec(double mu, const nda::array<ComplexType, 4> &spectra,
   nda::array<ComplexType, 2> Xt(FT.nt_f(), _ns);
   nda::array<ComplexType, 1> nelecs(_ns);
   auto k_weight = mf.k_weight();
-  double scl = (_ns == 1 and mf.npol() == 1 ? 2.0 : 1.0); 
+  double scl = (_ns == 1 and mf.npol() == 1 ? 2.0 : 1.0);
 
   for (size_t n = 0; n < _nw; ++n) {
     long wn = FT.wn_mesh()(n);
